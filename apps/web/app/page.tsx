f04b2e4a9f9ff5aaa5de48bc639f343b409ff875
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2025-03-18 20:00:02 +0100

    feat(mobile): Adds working version of mobile

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import PageWrapper from '@/components/layout/page-wrapper'
 export default function Home() {
   return (
       <PageWrapper>
-        <span className="mt-2 bg-primary p-10 rounded-md text-primary-foreground w-full">Hello MatchCenterMedia!</span>
+        <span className="mt-2 bg-primary p-10 rounded-md text-primary-foreground w-full hover:bg-amber-950 transition-colors duration-1000">Hello MatchCenterMedia!</span>
       </PageWrapper>
   )
 }
